bca29a1d491e25edb4a23331e412ca4951df05ac
Nishant <nishantagrwal12993@gmail.com>
2018-10-19 04:50:36 +0530

    Added Semantic UI

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,5 +4,6 @@ import ReactDom from 'react-dom'
 import MainContainer from './components/MainContainer'
 
 import './style.css'
+import 'semantic-ui-css/semantic.min.css'
 
 ReactDom.render(<MainContainer/>, document.getElementById('app'))
