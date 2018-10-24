6773588191974231709a050dda17ba3c672a24e4
Nishant <nishantagrwal12993@gmail.com>
2018-10-25 03:01:15 +0530

    Fixed the unintended hover effect on the task cards

--- frontend/src/components/TasksCard.tsx
+++ frontend/src/components/TasksCard.tsx
@@ -13,7 +13,8 @@ const TaskCard = ({name, desc, onClick}: TaskCardProps) =>
         style={{width: 'auto'}} 
         header={name} 
         description={desc} 
-        onClick={() => onClick && onClick()} 
+        onClick={() => onClick && onClick()}
+        as="div"  // Needed because specifying onClick turns our Card into an <a> tag
     />
 
 export default TaskCard
