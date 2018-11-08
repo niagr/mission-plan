9bec430104f59cc6872a2b3490e8dbbce393a224
Nishant <nishantagrwal12993@gmail.com>
2018-11-08 21:07:27 +0530

    Include common drf settings in prod settings

--- backend/missioncontrol/settings_prod.py
+++ backend/missioncontrol/settings_prod.py
@@ -1,8 +1,10 @@
 from .settings import *
+from .settings import REST_FRAMEWORK as REST_FRAMEWORK_COMMON
 
 DEBUG = False
 
 REST_FRAMEWORK = {
+    **REST_FRAMEWORK_COMMON,
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
     )
